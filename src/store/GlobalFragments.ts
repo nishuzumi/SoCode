3d14dbae0cc80c53536eb9a1064f694060fb29b9
nishuzumi <2300206764@qq.com>
2024-11-16 11:52:19 +0700

    add vm class for souce compile

---
+++ src/store/GlobalFragments.ts
@@ -0,0 +1,223 @@
+import { CompileError, DecodeVariableResult } from "@/types";
+import { atom } from "jotai";
+import { atomWithImmer } from "jotai-immer";
+import { nanoid } from "nanoid";
+import { splitAtom } from "jotai/utils";
+
+export type FragmentCodeDetail = {
+    runnableCode?: string;
+    topLevelCode?: string;
+    globalCode?: string;
+};
+export enum RunnedStatus {
+    NotRunned,
+    Running,
+    Success,
+    Error,
+}
+// 定义一个类型来表示每个代码段的数据
+export type FragmentData = {
+    uuid: string;
+    code: string;
+    runIndex?: number;
+    detailCode?: FragmentCodeDetail;
+    result?: DecodeVariableResult;
+    error?: CompileError[];
+    scrollTo?: (position?: "result" | "code") => void;
+};
+
+export type FragmentSnapshot = FragmentData & {
+    source: Source;
+};
+
+export const RunIndexAtom = atom<number>(1);
+RunIndexAtom.debugLabel = "RunIndexAtom";
+
+export const FragmentsSnapshot = atom<FragmentSnapshot[]>([]);
+FragmentsSnapshot.debugLabel = "FragmentsSnapshot";
+
+export const LastFragmentSnapshot = atom<FragmentSnapshot | undefined>(
+    (get) => {
+        const fragments = get(FragmentsSnapshot);
+        if (fragments.length === 0) return undefined;
+        return fragments[fragments.length - 1];
+    }
+);
+
+export const FragmentsAtom = atomWithImmer<FragmentData[]>([
+    {
+        uuid: nanoid(),
+        code: "uint x = 10;\nx",
+    },
+    {
+        uuid: nanoid(),
+        code: `//:GlobalCode
+  abstract contract ERC20 {
+      /*//////////////////////////////////////////////////////////////
+                                   EVENTS
+      //////////////////////////////////////////////////////////////*/
+  
+      event Transfer(address indexed from, address indexed to, uint256 amount);
+  
+      event Approval(address indexed owner, address indexed spender, uint256 amount);
+  
+      /*//////////////////////////////////////////////////////////////
+                              METADATA STORAGE
+      //////////////////////////////////////////////////////////////*/
+  
+      string public name;
+  
+      string public symbol;
+  
+      uint8 public immutable decimals;
+  
+      /*//////////////////////////////////////////////////////////////
+                                ERC20 STORAGE
+      //////////////////////////////////////////////////////////////*/
+  
+      uint256 public totalSupply;
+  
+      mapping(address => uint256) public balanceOf;
+  
+      mapping(address => mapping(address => uint256)) public allowance;
+  
+      /*//////////////////////////////////////////////////////////////
+                              EIP-2612 STORAGE
+      //////////////////////////////////////////////////////////////*/
+  
+      uint256 internal immutable INITIAL_CHAIN_ID;
+  
+      bytes32 internal immutable INITIAL_DOMAIN_SEPARATOR;
+  
+      mapping(address => uint256) public nonces;
+  
+      /*//////////////////////////////////////////////////////////////
+                                 CONSTRUCTOR
+      //////////////////////////////////////////////////////////////*/
+  
+      constructor(
+          string memory _name,
+          string memory _symbol,
+          uint8 _decimals
+      ) {
+          name = _name;
+          symbol = _symbol;
+          decimals = _decimals;
+  
+          INITIAL_CHAIN_ID = block.chainid;
+          INITIAL_DOMAIN_SEPARATOR = computeDomainSeparator();
+      }
+  
+      /*//////////////////////////////////////////////////////////////
+                                 ERC20 LOGIC
+      //////////////////////////////////////////////////////////////*/
+  
+      function approve(address spender, uint256 amount) public virtual returns (bool) {
+          allowance[msg.sender][spender] = amount;
+  
+          emit Approval(msg.sender, spender, amount);
+  
+          return true;
+      }
+  
+      function transfer(address to, uint256 amount) public virtual returns (bool) {
+          balanceOf[msg.sender] -= amount;
+  
+          // Cannot overflow because the sum of all user
+          // balances can't exceed the max uint256 value.
+          unchecked {
+              balanceOf[to] += amount;
+          }
+  
+          emit Transfer(msg.sender, to, amount);
+  
+          return true;
+      }
+  
+      function transferFrom(
+          address from,
+          address to,
+          uint256 amount
+      ) public virtual returns (bool) {
+          uint256 allowed = allowance[from][msg.sender]; // Saves gas for limited approvals.
+  
+          if (allowed != type(uint256).max) allowance[from][msg.sender] = allowed - amount;
+  
+          balanceOf[from] -= amount;
+  
+          // Cannot overflow because the sum of all user
+          // balances can't exceed the max uint256 value.
+          unchecked {
+              balanceOf[to] += amount;
+          }
+  
+          emit Transfer(from, to, amount);
+  
+          return true;
+      }
+  
+      /*//////////////////////////////////////////////////////////////
+                               EIP-2612 LOGIC
+      //////////////////////////////////////////////////////////////*/
+      function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
+          return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
+      }
+  
+      function computeDomainSeparator() internal view virtual returns (bytes32) {
+          return
+              keccak256(
+                  abi.encode(
+                      keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                      keccak256(bytes(name)),
+                      keccak256("1"),
+                      block.chainid,
+                      address(this)
+                  )
+              );
+      }
+  
+      /*//////////////////////////////////////////////////////////////
+                          INTERNAL MINT/BURN LOGIC
+      //////////////////////////////////////////////////////////////*/
+  
+      function _mint(address to, uint256 amount) internal virtual {
+          totalSupply += amount;
+  
+          // Cannot overflow because the sum of all user
+          // balances can't exceed the max uint256 value.
+          unchecked {
+              balanceOf[to] += amount;
+          }
+  
+          emit Transfer(address(0), to, amount);
+      }
+  
+      function _burn(address from, uint256 amount) internal virtual {
+          balanceOf[from] -= amount;
+  
+          // Cannot underflow because a user's balance
+          // will never be larger than the total supply.
+          unchecked {
+              totalSupply -= amount;
+          }
+  
+          emit Transfer(from, address(0), amount);
+      }
+  }
+  
+  contract A is ERC20{
+    constructor() ERC20("A","a",18){
+      _mint(msg.sender,1000 ether);
+    }
+  }`,
+    },
+    {
+        uuid: nanoid(),
+        code: `A a = new A();
+  a.balanceOf(address(this))`,
+    }
+]);
+FragmentsAtom.debugLabel = "FragmentsAtom";
+
+export const FragmentAtomsAtom = splitAtom(FragmentsAtom);
+FragmentAtomsAtom.debugLabel = "FragmentAtomsAtom"
