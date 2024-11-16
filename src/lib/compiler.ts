ab2825376a80c9ef3d66466efc67b1e2635cf825
nishuzumi <2300206764@qq.com>
2024-11-16 12:18:58 +0700

    add compiler

---
+++ src/lib/compiler.ts
@@ -0,0 +1,42 @@
+import { createCompileInput } from './utils';
+let currentId = 0;
+const worker = new Worker(
+    new URL("./browser.solidity.worker.ts", import.meta.url), {
+    type: 'classic'
+}
+);
+
+export const solidityCompiler = async ({
+    version,
+    contractBody,
+    options,
+}: {
+    version: string;
+    contractBody: string;
+    options?: { optimizer?: { enabled: boolean; runs: number } };
+}) => {
+    const input = createCompileInput(contractBody, options);
+    const id = currentId++;
+
+    return new Promise((resolve, reject) => {
+        const handleMessage = ({ data }: MessageEvent) => {
+            const { id: responseId, result } = data;
+            if (responseId === id) {
+                worker.removeEventListener('message', handleMessage);
+                worker.removeEventListener('error', handleError);
+                resolve(result);
+            }
+        };
+
+        const handleError = (err: ErrorEvent) => {
+            worker.removeEventListener('message', handleMessage);
+            worker.removeEventListener('error', handleError);
+            reject(err);
+        };
+
+        worker.addEventListener('message', handleMessage);
+        worker.addEventListener('error', handleError);
+
+        worker.postMessage({ id, input, version });
+    });
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const createCompileInput = (contractBody: string, options: object = {}): string => {
+  const CompileInput = {
+    language: 'Solidity',
+    sources: {
+      'Compiled_Contracts': {
+        content: contractBody
+      }
+    },
+    settings: {
+      ...options,
+      outputSelection: {
+        '*': {
+          '*': ['*'],
+          "": [
+            "ast"
+          ]
+        },
+      },
+    },
+  };
+  return JSON.stringify(CompileInput);
+}
